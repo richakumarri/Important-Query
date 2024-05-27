f54cc5f1485532f44fefcd2ae26fd4e78cb5a4df
richakumarri <167754739+richakumarri@users.noreply.github.com>
2024-05-27 16:01:27 +0400

    Update 0 TP_base.sql

--- 0 TP_base.sql
+++ 0 TP_base.sql
@@ -1,13 +1,18 @@
-create table scratch.riders.TP_master_table as 
-with base as(
+create  or replace table scratch.riders.TP_master_table as 
+ 
+with base as
+(
 select 
  distinct 
  zone_code
  ,city_name
-from PRODUCTION.AGGREGATE.AGG_ZONE_DELIVERY_METRICS_HOURLY
+ ,CAST(EXTRACT(HOUR FROM START_OF_PERIOD_LOCAL) AS INT) as hour_of_day
+from 
+PRODUCTION.AGGREGATE.AGG_ZONE_DELIVERY_METRICS_HOURLY
 where
-date (start_of_period_local) =  current_date()
-and CNT_ERAT >0 
+date (start_of_period_local) between   current_date-30 and current_date
+and is_within_zone_hours =true
+and CNT_ERAT >0  
 )
 , base_2 as(
  select 
@@ -19,17 +24,10 @@ where
 date (start_of_period_local) between   '2023-03-01' and '2024-04-30'
 and CNT_ERAT >0 
 )
-, distinct_hr as(
-select distinct hour_of_Day from base_2
-)
-, distinct_month as(
+,distinct_month as(
 select distinct order_month from base_2
 )
-
- select * from base 
- left join distinct_hr
- on 1=1
- left join 
- distinct_month 
- on 1=1
-
+select * from base 
+left join 
+distinct_month 
+ on 1=1;
